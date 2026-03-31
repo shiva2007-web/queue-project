fbf2b37bc62b66e7622e200b99369a40e4aa1a14
Gnanashiva <gnanashiva07@gmail.com>
2026-03-31 12:06:28 +0530

    Adding Location check

--- server.js
+++ server.js
@@ -6,16 +6,21 @@ app.use(cors());
 app.use(express.json());
 
 let queue = [];
-
-// Join queue
+function distance(lat1, lon1, lat2, lon2) {
+    return Math.sqrt(
+        Math.pow(lat1 - lat2, 2) + Math.pow(lon1 - lon2, 2)
+    );
+}
 app.post("/join", (req, res) => {
     const user = {
         id: Date.now(),
         name: req.body.name,
+        lat: req.body.lat,
+        lon: req.body.lon,
         lastActive: Date.now()
     };
     queue.push(user);
-    res.json({ position: queue.length,id: user.id });
+    res.json({ position: queue.length, id: user.id });
 });
 
 // Update activity (heartbeat)
@@ -30,12 +35,18 @@ app.post("/active", (req, res) => {
     res.json({ message: "updated" });
 });
 
-// Get queue (remove inactive users)
 app.get("/queue", (req, res) => {
     const now = Date.now();
 
-    // remove users inactive for 10 seconds
-    queue = queue.filter(u => now - u.lastActive < 10000);
+    const CENTER_LAT = 12.9716; // set your location
+    const CENTER_LON = 77.5946;
+
+    queue = queue.filter(u => {
+        const active = now - u.lastActive < 10000;
+        const near = distance(u.lat, u.lon, CENTER_LAT, CENTER_LON) < 0.01;
+
+        return active && near;
+    });
 
     res.json(queue);
 });
